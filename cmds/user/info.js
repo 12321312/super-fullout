47cac0d3a038a4150d901c2b44cebb20e23f5099
12321312 <xshadownightx12@gmail.com>
2019-10-22 17:15:15 +0700

    Update info.js

--- cmds/user/info.js
+++ cmds/user/info.js
@@ -25,7 +25,7 @@ exports.run = async (bot, message, args) => {
 };
 module.exports.command = {
   name: 'info',
-  aliases: ["серверинфо", "инфосервер", "инфопросервер"],
+  aliases: ["серверинфо", "инфосервер", "инфопросервер", "инфо"],
   description: "Инфа о сервере, чо доебался?",
   usage: "usercommand",
   category: "user",
