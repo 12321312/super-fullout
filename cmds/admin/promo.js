a4650f63f8a206c3c13bb92ad65b0f82aff0ad74
12321312 <xshadownightx12@gmail.com>
2020-02-14 23:23:02 +0700

    Update promo.js

--- cmds/admin/promo.js
+++ cmds/admin/promo.js
@@ -37,7 +37,7 @@ connection.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
     sql = `UPDATE xp SET xp,point = 7500,5 WHERE id = '${target.id}'`  
     //bot.send(`Изменил кол-во XP у пользователя <@${target.id}>. \nВсего опыта у пользователя: ` + `\`\`\`js\n${args[1]} XP\`\`\``);
     target.addRole(promorole.id);
-
+    };
 
     connection.query(sql);
 });
