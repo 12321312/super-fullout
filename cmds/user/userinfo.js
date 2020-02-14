46ff65f0f6de8fcddc02b35c8757f60f70fd3df7
12321312 <xshadownightx12@gmail.com>
2020-02-15 00:20:37 +0700

    Update userinfo.js

--- cmds/user/userinfo.js
+++ cmds/user/userinfo.js
@@ -51,7 +51,7 @@ exports.run = async (bot,message,args,connection) => {
     .addField("Донат поинтов:",point, true)
     .addField("Звание:",zhanei, true)
     .addField("ID индификатор:",a.id, true);
-    if(fallo === 1) ambed.addField("**Участник промо акции.**", "", true);
+    if(fallo === 1) ambed.addField("**Участник промо акции.**", "+", true);
     if(keys) ambed.addField("Ключи:", keys, true);
     if(warn && !warn2) ambed.addField("Варны:", `**1**: ${warn}`, true);
     if(warn2 && !warn3) ambed.addField("Варны:", `**1**: ${warn}\n**2**: ${warn2}`, true);
