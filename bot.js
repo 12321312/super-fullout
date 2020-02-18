e4530ad63573da53c2e33901b5737d8147f46576
12321312 <xshadownightx12@gmail.com>
2020-02-18 11:38:30 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -94,8 +94,7 @@ process.on('unhandledRejection', err => {
 });
 
 function update() {
-    let sizemem = bot.guild.members.size;
-    bot.guild.channels.get("679181672482209840").setName(`$Всего участников}:${sizemem}`);
+    bot.guild.channels.get("679181672482209840").setName(`$Всего участников}:0`);
 }
 
 // При загузке
