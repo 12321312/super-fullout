06cff1e8010866b1741b8091b2d4b49a65e2d7cc
12321312 <xshadownightx12@gmail.com>
2020-03-13 11:46:44 +0700

    Create gamestats.js

---
+++ cmds/superadmin/gamestats.js
@@ -0,0 +1,15 @@
+const Discord = module.require("discord.js");
+const fs = require("fs");
+exports.run = async (bot, message, args) => { 
+    if(!message.member.roles.some(r=>["Розовое чудо"].includes(r.name))) return message.reply('Отказано в доступе.');
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);        
+    console.log(member.presence.game.state);
+};
+module.exports.command = {
+    name: 'gamestats',
+    aliases: ["статс", "тестстатус", "тс"],
+    description: "Отправляет в канал какую-то дичь, чо доебался?",
+    usage: "foxcommand",
+    category: "fox",
+    enabled: false
+}; 
