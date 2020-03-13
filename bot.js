fefb28d37bec81f268574d9d5e37c011b09a236e
12321312 <xshadownightx12@gmail.com>
2020-03-13 13:22:30 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,11 +100,10 @@ async function update() {
     let vflolow = bot.users.filter(m => m.presence.game != null && m.presence.game.type == 0 && m.presence.game.name === 'Fallout 76').size;
     let voiceChannels = bot.channels.filter(c => c.type === 'voice');
     let count = 0;
-    let altolow = 0;
     for (const [id, voiceChannel] of voiceChannels) count += voiceChannel.members.size;
     let voiceChs = bot.guilds.get('584827387607515137').channels.filter(c => c.id === '628604126149869569' || c.id === '584832686208843826' || c.id === '584832761123307541' || c.id === '584832781201309717' || c.id === '629554985331326996' || c.id === '629555066537508894' || c.id === '629555098523271178' || c.id === '629554945359872020')
     voiceChs.forEach(function(c, i) {
-    altolow = bot.guilds.get('584827387607515137').channels.get(c.id).members.filter(m => m.presence.game != null && m.presence.game.type == 0 && m.presence.game.name === 'ATLAS')
+    var altolow = bot.guilds.get('584827387607515137').channels.get(c.id).members.filter(m => m.presence.game != null && m.presence.game.type == 0 && m.presence.game.name === 'ATLAS')
     });
     let xip = await superagent
     .get(`https://api.bethesda.net/status/ext-server-status?product_id=8`);
@@ -131,11 +130,11 @@ async function update() {
     } else {
      bot.channels.get("679187372100812800").setName(`📎Сервера Fallout: ⛔`);
     };
-    if(altolow != 0) {
+  
     altolow.forEach(function(m, i) {
       m.setVoiceChannel('671295673865601025');
      })
-    };
+
 }
 
 // При загузке
