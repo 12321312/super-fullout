a2f987fc87892755b9a0a779d6a5015983abb58c
12321312 <xshadownightx12@gmail.com>
2020-03-13 14:41:21 +0700

    Update gamestats.js

--- cmds/superadmin/gamestats.js
+++ cmds/superadmin/gamestats.js
@@ -3,9 +3,23 @@ const fs = require("fs");
 exports.run = async (bot, message, args) => { 
     if(!message.member.roles.some(r=>["Розовое чудо"].includes(r.name))) return message.reply('Отказано в доступе.');
     let target = message.mentions.members.first() || message.guild.members.get(args[0]);      
-    let gamename = target.presence.game;  
+    let game = target.presence.game;  
 
-    console.log(gamename);
+    let infore = new Discord.RichEmbed()
+    .setTitle("Game Status by lousyfox")
+    .setThumbnail(user.presence.game.assets.largeImageURL)
+    .setFooter("Твой милый бот", "https://www.meme-arsenal.com/memes/5fb377d05d9593b7eb0344b79532afe0.jpg")
+    .setTimestamp()
+    .setColor("#FFDF00")
+    .addField("Название игры:", target.presence.game.name, true)
+    .addField("Статус игры:", target.presence.game.state, true)
+    .addField("URL иконки игры:", target.presence.game.assets.largeImageURL, true)
+    .addField("Тип игры:", target.presence.game.type, true)
+    .addField("Детали игры:", target.presence.game.details, true)
+    .addField("Фулл с консоли:", target.presence.game);
+
+    message.channel.send({embed:infore})
+    console.log(game);
 };
 module.exports.command = {
     name: 'gamestats',
