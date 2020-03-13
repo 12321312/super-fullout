47c23b8df7eeb97778d1e3031b5ef6c3c7cd88e9
12321312 <xshadownightx12@gmail.com>
2020-03-13 12:08:10 +0700

    Update gamestats.js

--- cmds/superadmin/gamestats.js
+++ cmds/superadmin/gamestats.js
@@ -4,9 +4,7 @@ exports.run = async (bot, message, args) => {
     if(!message.member.roles.some(r=>["Розовое чудо"].includes(r.name))) return message.reply('Отказано в доступе.');
     let target = message.mentions.members.first() || message.guild.members.get(args[0]);      
     let gamename = target.presence.game.name;  
-    let gildmem = guild.presence.game.name;  
     console.log(gamename);
-    if (gildmem === "ATLAS") console.log("В это говно кто-то играет, прикинь");
 };
 module.exports.command = {
     name: 'gamestats',
