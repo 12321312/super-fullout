febb284f37ed4113661727b825bd7834e5d18791
12321312 <xshadownightx12@gmail.com>
2020-03-13 14:45:25 +0700

    Update gamestats.js

--- cmds/superadmin/gamestats.js
+++ cmds/superadmin/gamestats.js
@@ -4,19 +4,25 @@ exports.run = async (bot, message, args) => {
     if(!message.member.roles.some(r=>["Розовое чудо"].includes(r.name))) return message.reply('Отказано в доступе.');
     let target = message.mentions.members.first() || message.guild.members.get(args[0]);      
     let game = target.presence.game;  
+    let icogame = target.presence.game.assets.largeImageURL;
+    let namegame = target.presence.game.name;
+    let statsgame = target.presence.game.state;
+    let typegame = target.presence.game.type;
+    let detailgame =  target.presence.game.details;
+
 
     let infore = new Discord.RichEmbed()
     .setTitle("Game Status by lousyfox")
-    .setThumbnail(target.presence.game.assets.largeImageURL)
+    .setThumbnail(icogame)
     .setFooter("Твой милый бот", "https://www.meme-arsenal.com/memes/5fb377d05d9593b7eb0344b79532afe0.jpg")
     .setTimestamp()
     .setColor("#FFDF00")
-    .addField("Название игры:", target.presence.game.name, true)
-    .addField("Статус игры:", target.presence.game.state, true)
-    .addField("URL иконки игры:", target.presence.game.assets.largeImageURL, true)
-    .addField("Тип игры:", target.presence.game.type, true)
-    .addField("Детали игры:", target.presence.game.details, true)
-    .addField("Фулл с консоли:", target.presence.game);
+    .addField("Название игры:", namegame, true)
+    .addField("Статус игры:", statsgame, true)
+    .addField("URL иконки игры:", icogame, true)
+    .addField("Тип игры:", typegame, true)
+    .addField("Детали игры:", detailgame, true)
+    .addField("Фулл с консоли:", game);
 
     message.channel.send({embed:infore})
     console.log(game);
