40bb61f925a31d9a527bf008c156a27435bcb891
12321312 <xshadownightx12@gmail.com>
2020-03-14 17:20:49 +0700

    Update move.js

--- cmds/admin/move.js
+++ cmds/admin/move.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 exports.run = async (bot, message, args) => { 
     if(!message.member.roles.some(r=>["Розовое чудо"].includes(r.name))) return message.reply('Отказано в доступе.');  
     let targetChannels = bot.channels.get(args[0]);
-    let tarhet = targetChannels.member.user;
+    let tarhet = targetChannels.member;
     console.log(tarhet);
     message.channel.send(tarhet);
 /*   
