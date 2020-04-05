e2d239827062a6b0f141d387d44dc59d5e886c0a
12321312 <xshadownightx12@gmail.com>
2020-04-06 04:12:02 +0700

    Update roll.js

--- cmds/user/roll.js
+++ cmds/user/roll.js
@@ -4,9 +4,9 @@ exports.run = async (bot, message, args) => {
 let randonciks = Math.floor(Math.random() * 9999999999999) + 1 ;
 let randonnaplrr = Math.floor(Math.random() * (args[0])) + 1 ;   
 
-if (isNaN(args[0])) { message.reply("Ты бы число указывал, да? Откуда мне брать его? Ну на рандомное: **"+randonciks+"**, первое что на ум пришло."); return;}
-if ((args[0]) >= 100000) { message.reply('А не жирно будет? Го меньше символов.'); return;}
-if (1 >= (args[0])) { message.reply('А не ну збс, результат сам знаешь.'); return;}
+if (isNaN(args[0])) return message.reply("Ты бы число указывал, да? Откуда мне брать его? Ну на рандомное: **"+randonciks+"**, первое что на ум пришло."); 
+if ((args[0]) >= 100000) return message.reply('А не жирно будет? Го меньше символов.');
+if (1 >= (args[0])) return message.reply('А не ну збс, результат сам знаешь.');
 message.reply("**" + randonnaplrr + "**"); 
 };
 module.exports.command = {
